d696c63a7512b9b5f4e23bc31b2090bfd54e0019
Michael Rodriguez <mikeyrod22@gmail.com>
2021-09-11 18:07:45 -0500

    updated questions, added favicon

---
+++ assets/js/questions.js
@@ -0,0 +1,96 @@
+const questions = [
+  {
+    question: "How you properly link a stylesheet to an HTML page?",
+    answers: [
+      'A) <link rel="stylesheet" href="./style.css" />',
+      'B) <link rel="stylesheet" src="./style.css" />',
+      'C) <style type="css" href="./style.css" />',
+      'D) <a rel="stylesheet" href="./style.css" />',
+    ],
+    correct: "A",
+  },
+  {
+    question:
+      "How would you properly access the third item of an array named 'myArray'?",
+    answers: [
+      "A) myArray(3)",
+      "B) myArray[3]",
+      "C) myArray [2]",
+      "D) myArray.3",
+    ],
+    correct: "C",
+  },
+  {
+    question:
+      "Which of the following values is considered truthy in javascript?",
+    answers: ["A) undefined", 'B) "0"', "C) 0", 'D) ""'],
+    correct: "B",
+  },
+  {
+    question: "What is a regular expression?",
+    answers: [
+      "A) A pattern for matching characters or groups of characters in strings.",
+      "B) Any singular arithmetic operation, such as multiplying a number by another number.",
+      "C) A function that accepts no arguments and returns a primitive data type, such as a number, string, or boolean.",
+      "D) A standard of practice in which developers aim to declare variables using clear, easy to read names as opposed to ambiguous, arbitrary characters.",
+    ],
+    correct: "A",
+  },
+  {
+    question:
+      "Which of the following time complexities is most efficient as input size increases?",
+    answers: ["A) O(n)", "B) O(log n)", "C) O(1)", "D) O(n log n)"],
+    correct: "C",
+  },
+  {
+    question: "Which of the following best describes Node.js?",
+    answers: [
+      "A) A Javascript library designed to more easily interface with the DOM.",
+      "B) A platform for hosting websites and web apps.",
+      "C) An external run time environment for executing Javascript code outside of the browser.",
+      "D) A popular blogposting site for Javascript developers.",
+    ],
+    correct: "C",
+  },
+  {
+    question: "What does ORM stand for?",
+    answers: [
+      "A) Objective Regulation Mechanism",
+      "B) Official React Manual",
+      "C) Online Runtime Manager",
+      "D) Object Relational Mapping",
+    ],
+    correct: "D",
+  },
+  {
+    question: "What is one advantage of using GraphQL ?",
+    answers: [
+      "A) You are able to display data from your server in a preformatted graph.",
+      "B) GraphQL is the only way to make requests to a database with a Node.js backend.",
+      "C) GraphQL is made by Facebook, so Facebook developers will routinely debug all of your GraphQL projects for you!",
+      "D) You are not restricted by API endpoints and can therefore access specific amounts and types of data in a single request.",
+    ],
+    correct: "D",
+  },
+  {
+    question:
+      "Which of the following is the best description of Responsive Web Design?",
+    answers: [
+      "A) Implementing user feedback and review functionality in all of your websites.",
+      "B) Designing features to be used only by authenticated users.",
+      "C) Designing your Website to dynamically change it's appearance based on things such as screen-size and orientation.",
+      "D) Ensuring every img attribute has an appropriate alt tag.",
+    ],
+    correct: "C",
+  },
+  {
+    question: "What is a Progressive Web Application?",
+    answers: [
+      "A) An app that must be downloaded on a mobile device through an app store.",
+      "B) An app that uses tools such as service workers and manifests to replicate functionalities of native apps.",
+      "C) An app that has received an award for exceptional creativity.",
+      "D) A term for an app that has met certain accessability criteria.",
+    ],
+    correct: "B",
+  },
+];
